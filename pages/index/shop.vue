82580b3aab2aabb72cc17af586752b2377ec7d26
ARES6541 <2314842317@qq.com>
2022-08-23 16:57:41 +0800

    git

---
+++ pages/index/shop.vue
@@ -0,0 +1,36 @@
+<template>
+	<div class="item">
+<!-- 		<van-card
+		  :num="shop.nice"
+		  :price="shop.price"
+		  desc="描述信息"  
+		  :title="shop.title"
+		  :thumb="shop.img_list_url"
+		/> -->
+	</div>
+</template>
+
+<script>
+	export default {
+		name:'Shop',
+		props: {
+			shop: {
+				type:Object,
+				default(){
+					return {
+						img_list_url:'@/static/logo.png',
+						id:123,
+						title:'商品名称',
+						price:'00'
+					}
+				}
+			}
+		}
+	}
+</script>
+
+<style scoped>
+	item{
+		display: inline-block;
+	}
+</style>
